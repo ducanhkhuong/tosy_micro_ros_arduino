f2a0fc540690743b2447dbf64434299929f7ada5
ducanhkhuong <duca3774@gmail.com>
2026-05-08 17:08:42 +0700

    rebuild

---
+++ src/include/tosy_robot_msgs/msg/joint_angle.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from tosy_robot_msgs:msg/JointAngle.idl
+// generated code does not contain a copyright notice
+
+#ifndef TOSY_ROBOT_MSGS__MSG__JOINT_ANGLE_H_
+#define TOSY_ROBOT_MSGS__MSG__JOINT_ANGLE_H_
+
+#include "tosy_robot_msgs/msg/detail/joint_angle__struct.h"
+#include "tosy_robot_msgs/msg/detail/joint_angle__functions.h"
+#include "tosy_robot_msgs/msg/detail/joint_angle__type_support.h"
+
+#endif  // TOSY_ROBOT_MSGS__MSG__JOINT_ANGLE_H_
